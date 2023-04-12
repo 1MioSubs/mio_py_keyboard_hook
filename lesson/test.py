9f030560079aae9d7bc8c9eb6e12948941f87293
1MioSubs <it09022022@gmail.com>
2023-04-12 16:26:39 +0300

    start proj 0.1

---
+++ lesson/test.py
@@ -0,0 +1,11 @@
+from pynput import keyboard
+
+
+def on_release(key):
+    print(f'{key} released')
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+with keyboard.Listener(on_release=on_release) as listener:
+    listener.join()
